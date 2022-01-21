cf41c3da0211c991c6681f23092432afd9e40807
Jan Starke <jan.starke@outofbed.org>
2022-01-21 18:10:41 +0100

    initial usage info

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# dionysos
+# `dionysos`
 Scanner for certain IoCs
+
+# Usage
+```
+dionysos 0.1.0
+
+Scanner for certain IoCs
+
+USAGE:
+    dionysos [PATH]
+
+ARGS:
+    <PATH>    path to registry hive file
+
+OPTIONS:
+    -h, --help       Print help information
+    -V, --version    Print version information
+```
--- src/dionysos.rs
+++ src/dionysos.rs
@@ -1,15 +1,43 @@
 use anyhow::Result;
+use clap::{App, Arg};
+use std::path::{PathBuf};
 
 pub struct Dionysos {
-
+    path: PathBuf
 }
 
 impl Dionysos {
     pub fn new() -> Result<Self> {
-        Ok(Self{})
+        Self::parse_options()
     }
 
     pub fn run(&self) -> Result<()> {
         Ok(())
+    }
+
+    fn parse_options() -> Result<Self> {
+        let app = App::new(env!("CARGO_PKG_NAME"))
+            .version(env!("CARGO_PKG_VERSION"))
+            .author(env!("CARGO_PKG_AUTHORS"))
+            .about(env!("CARGO_PKG_DESCRIPTION"))
+            .arg(
+                Arg::new("PATH")
+                    .help("path to registry hive file")
+                    .required(false)
+                    .multiple_occurrences(false)
+                    .multiple_values(false)
+                    .takes_value(true),
+            )
+            ;
+        
+        let matches = app.get_matches();
+        let path = match matches.value_of("PATH") {
+            Some(path) => PathBuf::from(&path),
+            None => PathBuf::from("/"),
+        };
+
+        Ok(Self {
+            path
+        })
     }
 }
