09b158e581b6a4177d57432bb9a00acedc84bd9d
Jan Starke <jan.starke@t-systems.com>
2022-06-22 19:01:11 +0200

    switch to different docker image

--- build/pre.sh
+++ build/pre.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-echo "updating Rust build toolchain"
-echo "============================================"
-rustup update stable
-rustup default stable
-rustup target add x86_64-unknown-linux-musl
-rustup show
-echo "============================================"
-
 echo "updating package metadata"
 echo "============================================"
 apt update
