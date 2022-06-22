8314fa0e6343d71dc753b79d1fe1b76806460f47
Jan Starke <jan.starke@t-systems.com>
2022-06-22 17:21:05 +0200

    added informational messages

--- build/pre.sh
+++ build/pre.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
-rustup show
+echo "updating Rust build environment"
+echo "============================================"
 rustup update stable
+rustup show
+echo "============================================"
+
+echo "updating package metadata"
+echo "============================================"
+apt update
+echo "============================================"
+
+echo "installing missing packages"
 apt-get install -y libyara-dev libclang-dev
+echo "============================================"
