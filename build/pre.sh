b522cc72b48a1e2b9dbb398af23c5c3892940a0f
Jan Starke <jan.starke@t-systems.com>
2022-06-23 10:59:04 +0200

    added deb dependencies

--- build/pre.sh
+++ build/pre.sh
@@ -11,6 +11,22 @@ echo "============================================"
 
 echo "installing missing packages"
 if [ -f /etc/alpine-release ]; then
+    apk add autoconf automake libtool bison
+    git clone https://github.com/VirusTotal/yara.git
+    cd yara.git
+    git checkout v4.2.1
+    ./bootstrap.sh
+    ./configure --enable-static
+    make && make install
+    cd ..
+
+    curl https://astron.com/pub/file/file-5.42.tar.gz --output - | tar xz
+    cd file-5.42
+    ./configure --enable-static && make && make install
+    cd ..
+
+    cd ..
+
     apk add clang-static yara-dev clang-dev
 else
     apt-get install -y libyara-dev clang
